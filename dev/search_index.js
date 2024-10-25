6a721c6af64cfc4b3ef9eff65aeeedd207fa85c6
Documenter.jl <documenter@juliadocs.github.io>
2024-10-25 14:53:54 +0000

    build based on 22782b5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuliaGeo","category":"page"},{"location":"#JuliaGeo","page":"Home","title":"JuliaGeo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JuliaGeo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuliaGeo]","category":"page"}]
+}
