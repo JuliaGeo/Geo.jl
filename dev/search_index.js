503e7ede31428107ceb3a80c2fc93cf0970ed4cd
Documenter.jl <documenter@juliadocs.github.io>
2024-12-13 20:35:27 +0000

    build based on 10ea0cf

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuliaGeo","category":"page"},{"location":"#JuliaGeo","page":"Home","title":"JuliaGeo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JuliaGeo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuliaGeo]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Geo","category":"page"},{"location":"#Geo","page":"Home","title":"Geo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Geo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Geo]","category":"page"}]
 }
