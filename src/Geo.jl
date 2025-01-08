0968360133e4c7cf202980e87798669f3a8f8d82
Rafael Schouten <rafaelschouten@gmail.com>
2025-01-08 01:53:14 +0100

    add packages

--- src/Geo.jl
+++ src/Geo.jl
@@ -1,5 +1,17 @@
 module Geo
 
-# Write your package code here.
+using ArchGDAL
+using Extents
+using GeoDataFrames
+using GeoFormatTypes
+using GeoInterface
+using GeoInterfaceMakie
+using GeoInterfaceRecipes
+using GeometryOps
+using LibGEOS
+using NCDatasets
+using Proj
+using Rasters
+using ZarrDatasets
 
 end
