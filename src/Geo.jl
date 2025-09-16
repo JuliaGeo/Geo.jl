36c6559b6c90d6f9f1b1e67e375eb64264bc1107
Alex Gardner <alex.s.gardner@jpl.nasa.gov>
2025-09-16 16:17:18 -0700

    Attempt to get intelegent tab completion

--- src/Geo.jl
+++ src/Geo.jl
@@ -1,21 +1,30 @@
 module Geo
 
-using DimensionalData
-using Extents
-using GeoDataFrames
-using GeoFormatTypes
-using GeoInterface
-using GeometryOps
-using Proj
-using Rasters
+    using DimensionalData 
+    using GeoDataFrames
+    using GeoFormatTypes
+    using GeoInterface
+    using GeometryOps
+    using Extents
+    using Proj
+    using Rasters
 
-import ArchGDAL
-import GeoJSON
-import GeoParquet
-import NCDatasets
-import LibGEOS
-import Shapefile
-import ZarrDatasets
-import WellKnownGeometry
+    import ArchGDAL
+    import GeoJSON
+    import GeoParquet
+    import NCDatasets
+    import LibGEOS
+    import Shapefile
+    import ZarrDatasets
+    import WellKnownGeometry
+
+    import DimensionalData as DD
+    import GeoDataFrames as GDF
+    import GeoFormatTypes as GFT
+    import GeoInterface as GI
+    import GeometryOps as GO
+
+    export DD, GDF, GFT, GO, GI, Extents, Proj, Rasters
+
 
 end
