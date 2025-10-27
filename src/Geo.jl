d6e5d2dd580ce0d36050ff4287fcc69c46cb44c8
Rafael Schouten <rafaelschouten@gmail.com>
2025-10-28 02:35:21 +1100

    mark a lot of methods as public, and reorganise

--- src/Geo.jl
+++ src/Geo.jl
@@ -1,5 +1,6 @@
 module Geo
 
+# Core tools with names you can access with e.g. Geo.func
 using DimensionalData
 using Extents
 using GeoDataFrames
@@ -8,14 +9,45 @@ using GeoInterface
 using GeometryOps
 using Proj
 using Rasters
+using GeoInterface.Wrappers
 
-import ArchGDAL
+# These packages are used under the hood for geometry data
 import GeoJSON
 import GeoParquet
-import NCDatasets
-import LibGEOS
 import Shapefile
-import ZarrDatasets
 import WellKnownGeometry
+
+# These are for loading raster/gridded data
+import NCDatasets
+import ZarrDatasets
+
+# ArchGDAL is used for everything
+import ArchGDAL
+
+# LibGeos is used for some additional methods GeometryOps doesn't have yet
+import LibGEOS
+
+# Clarifications: where multiple names are available
+using GeoDataFrames: read
+using GeoInterface: convert, extent # This extent knows how to calculate when not availablae
+
+# public
+# Mark all the names from `using Package` packages as public
+# This means accessing the docs wont have a warning.
+macro mark_public(modulename::Symbol)
+    mod = getfield(Geo, modulename)
+    namesvec = names(mod)
+    return Expr(:public, esc.(namesvec)...) 
+end
+
+@mark_public DimensionalData
+@mark_public Extents
+@mark_public GeoDataFrames
+@mark_public GeoFormatTypes
+@mark_public GeoInterface
+@mark_public GeometryOps
+@mark_public Proj
+@mark_public Rasters
+@mark_public Wrappers # GeoInterface.Wrappers
 
 end
