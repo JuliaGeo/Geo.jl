ba45a62bc04f5b91d620d408b5108216b23f485e
Documenter.jl <documenter@juliadocs.github.io>
2025-11-24 11:02:52 +0000

    build based on a8f57b9

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#Geo","page":"Home","title":"Geo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Geo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#Geo","page":"Home","title":"Geo","text":"Documentation for Geo.\n\n","category":"section"}]
 }
